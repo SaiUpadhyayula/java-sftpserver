bb09f0f8b2f3ce46dbeff23985c49a5875d1bd7f
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2019-02-23 13:03:48 +0100

    initial commit

---
+++ src/main/java/com/programming/techie/javasftpserver/JavaSftpserverApplication.java
@@ -0,0 +1,14 @@
+package com.programming.techie.javasftpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSftpserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSftpserverApplication.class, args);
+		while (true);
+	}
+
+}
---
+++ src/main/java/com/programming/techie/javasftpserver/MySftpServer.java
@@ -0,0 +1,36 @@
+package com.programming.techie.javasftpserver;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.config.keys.AuthorizedKeysAuthenticator;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+@Service
+public class MySftpServer {
+
+    private Log log = LogFactory.getLog(MySftpServer.class);
+
+    @PostConstruct
+    public void startServer() throws IOException {
+        start();
+    }
+
+    private void start() throws IOException {
+        SshServer sshd = SshServer.setUpDefaultServer();
+        sshd.setHost("localhost");
+        sshd.setPort(2222);
+        sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(new File("host.ser")));
+        sshd.setSubsystemFactories(Collections.singletonList(new SftpSubsystemFactory()));
+        sshd.setPasswordAuthenticator((username, password, session) -> username.equals("test") && password.equals("password"));
+        sshd.start();
+        log.info("SFTP server started");
+    }
+}
